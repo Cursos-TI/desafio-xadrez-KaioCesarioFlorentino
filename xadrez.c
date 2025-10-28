64319f35fa4b98766f2db71810e90be876006e83
KaioCesarioFlorentino <kaiocesario2005@gmail.com>
2025-10-28 00:40:07 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,48 +1,60 @@
 #include <stdio.h>
 
+void moveTorre(int i){
+
+    if(i > 0){
+        printf("Direita\n");
+        moveTorre(i - 1);
+    }
+}
+
+void moveBispo(int loop){
+    if(loop > 0){
+        for(int i = 1; i > 0; i--){
+            for(int j = 1; j > 0; j--){
+                printf("Direita, ");
+            }
+            printf("Cima\n");
+        }
+        moveBispo(loop - 1);
+    }
+}
+
+void moveRainha(int i){
+    if(i > 0){
+        printf("Esquerda\n");
+        moveRainha(i - 1);
+    }
+}
+
+
 int main() {
-    
-    //Declarando as variáveis que vão formar os loops
-    int i = 1;
-    int o = 1;
-    int p;
-    int k;
-    int l = 1;
 
     printf("###Desafio Xadrez###\n\n");
 
-    //Criando a movimentação da torre com loop
+    //Criando a movimentação da torre com recursividade
     printf("\nMovimento da Torre:\n");
+    int i = 5;
+    moveTorre(i);
 
-    while(i <= 5){
-    printf("Direita\n");
-    i++;
-    }
-
-    //Criando a movimentação do bispo com loop
+    //Criando a movimentação do bispo com loops e recursividade
     printf("\nMovimento do Bispo:\n");
+    int loops = 5;
+    moveBispo(loops);
 
-    do{
-        printf("Cima, Direita\n");
-        o++;
-    } while(o <=5);
-
-    //Criando a movimentação da rainha com loop
+    //Criando a movimentação da rainha com recursividade
     printf("\nMovimento da Rainha:\n");
-
-    for(p = 1; p <= 8; p++){
-        printf("Esquerda\n");
-    }
+    int j = 8;
+    moveRainha(j);
 
     //Criando a movimentação do cavalo com loop aninhando
     printf("\nMovimento do Cavalo:\n");
-
-    for(k = 1; k < 2; k++){
-        while(l <= 2){
-            printf("Baixo\n");
-            l++;
+    int k, l;
+    for(k = 1; k > 0; k--){
+        for(l = 2; l > 0; l--){
+            printf("Cima\n");
         }
-        printf("Esquerda\n");
+        printf("Direita\n");
     }
 
 
